d9ed02149402b4f021abc5fb91c6ff666140ba36
jglozano151 <jglozano151@gmail.com>
2018-07-16 15:30:01 -0700

    continue on document editor

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Editor, EditorState, RichUtils} from 'draft-js';
+import DocumentView from './comp/documentview'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {editorState: EditorState.createEmpty()}
-    this.onChange = (editorState) => this.setState({editorState});
   }
-  _onBoldClick(e) {
-    e.preventDefault()
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
-    console.log(this.state.editorState)
-  }
+  // Sample placeholder code for now
+  docId = '597592c3223e6e806478d'
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
-      <button color = "primary" onMouseDown = {(e) => this._onBoldClick(e)}> Bold! </button>
-      <Editor editorState = {this.state.editorState} onChange = {this.onChange} />
+      <DocumentView docId = {this.docId}/>
     </div>);
   }
 }
