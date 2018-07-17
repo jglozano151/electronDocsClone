5494b350b1357dadf43d5b83ede36aaeb651e034
jglozano151 <jglozano151@gmail.com>
2018-07-16 17:43:41 -0700

    update text editor

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const io = require('socket.io')()
-const socket = io('http://localhost:8080')
+// const io = require('socket.io')()
+// const socket = io('http://localhost:8080')
 
 const Homepage = require('./comp/homepage').default
 const Signup = require('./comp/signup').default
@@ -13,25 +13,28 @@ export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      socket: socket,
+      socket: '', //socket,
       page: '',
       userId: '',
       docArr:''
     }
   }
 
-  componentDidMount() {
-    socket.on('homepage', () => this.setState({page:'homepage'}))
-    socket.on('signup', () => this.setState({page:'signup'}))
-    socket.on('docList', (obj) => this.setState({page:'docList', userId:obj.id, docArr:obj.docs}))
-    socket.on('error', (obj) => alert(obj.msg))
-  }
+  // componentDidMount() {
+  //   socket.on('homepage', () => this.setState({page:'homepage'}))
+  //   socket.on('signup', () => this.setState({page:'signup'}))
+  //   socket.on('docList', (obj) => this.setState({page:'docList', userId:obj.id, docArr:obj.docs}))
+  //   socket.on('error', (obj) => alert(obj.msg))
+  // }
 
   render() {
-    this.state.page === homepage ? <Homepage socket={this.state.socket} /> : null
-    this.state.page === signup ? <Signup socket={this.state.socket} /> : null
-    this.state.page === docList ? <DocList socket={this.state.socket} userId={this.state.userId} docArr={this.state.docArr}/> : null
-    this.state.page === documentview ? <DocumentView docId={this.state.docId} socket={this.state.socket} /> : null
+    // this.state.page === homepage ? <Homepage socket={this.state.socket} /> : null
+    // this.state.page === signup ? <Signup socket={this.state.socket} /> : null
+    // this.state.page === docList ? <DocList socket={this.state.socket} userId={this.state.userId} docArr={this.state.docArr}/> : null
+    // this.state.page === documentview ? <DocumentView docId={this.state.docId} socket={this.state.socket} /> : null
+    return (
+      <DocumentView />
+    )
   }
 
 }
